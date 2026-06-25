db6bc3320ca0251291c5d072566e866dd38ff724
sandeep kushwaha <sandeep188288@gmail.com>
2026-06-25 14:34:16 +0530

    Time: 0 ms (100%), Space: 42.7 MB (54.71%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        java.util.HashMap<Character, String> map = new java.util.HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            if (map.containsKey(ch)) {
+                if (!map.get(ch).equals(words[i])) {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(words[i])) {
+                    return false;
+                }
+                map.put(ch, words[i]);
+            }
+        }
+
+        return true;
+    }
+}
