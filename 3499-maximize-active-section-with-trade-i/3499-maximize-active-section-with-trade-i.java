69b72db26b3bdc3cff992131fc4c268cf21ecd18
Sandeep Kushwaha <sandeep188288@gmail.com>
2026-07-21 15:02:15 +0530

    Time: 82 ms (70.42%), Space: 47.9 MB (83.1%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -7,23 +7,18 @@ class Solution {
         int i = 0;
         while (i < s.length()) {
             int j = i;
-
             while (j < s.length() && s.charAt(j) == s.charAt(i)) {
                 j++;
             }
             int len = j - i;
-
             if (s.charAt(i) == '1') {
-                // Count all existing 1s
                 ones += len;
             } else {
                 if (prevZero > 0) {
                     maxZero = Math.max(maxZero, prevZero + len);
                 }
-
                 prevZero = len;
             }
-
             i = j;
         }
 
